84428863c0551983c48a020e3339a4e2f8496ab3
kukhtik <93156901+kukhtik@users.noreply.github.com>
2022-02-20 18:14:31 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,1 @@
-package com.company;
-
-public class Main {
-    public static void main(String[] args) {
-        String input = "camelCasing[]Test";
-        int afterC = input.indexOf('C');
-        int beforeC = input.indexOf('c');
-        int k = input.indexOf(']');
-        String finale = input.substring(afterC, afterC + 7);
-        String ff = input.substring(beforeC, afterC);
-        String fff = input.substring(k);
-        System.out.print(ff + " " + finale + " " + fff);
-    }
-}
 
